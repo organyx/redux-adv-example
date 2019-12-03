bf545cb9cf95291c846fe0982331f848c2d79714
Aleksandr Beliun <aleksandr.beliun@gmail.com>
2019-12-03 22:40:25 +0100

    Using utily funcs

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -1,6 +1,8 @@
 import * as actionTypes from './actionsTypes';
 
 export const saveResult = (res) => {
+  // const updatedResult = res * 2;
+  // ASYNC FUNC FOR THE MOST PAST
   return {
     type: actionTypes.STORE_RESULT,
     result: res
@@ -8,8 +10,11 @@ export const saveResult = (res) => {
 }
 
 export const storeResult = (res) => {
-  return (dispatch) => {
+  return (dispatch, getState) => {
     setTimeout(() => {
+      const oldCounter = getState().ctr.counter;
+      console.log(oldCounter);
+
       dispatch(saveResult(res));
     }, 2000);
   };
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,25 +1,30 @@
 import * as actionTypes from '../actions/actionsTypes';
+import { updateObject } from '../utility'
 
 const initialState = {
     results: []
 };
 
-const reducer = ( state = initialState, action ) => {
-    switch ( action.type ) {
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: action.result})
-            }
+            // CHANGE DATA HERE
+
+            return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result }) })
+        // return {
+        //     ...state,
+        //     results: state.results.concat({ id: new Date(), value: action.result })
+        // }
         case actionTypes.DELETE_RESULT:
             // const id = 2;
             // const newArray = [...state.results];
             // newArray.splice(id, 1)
             const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return {
-                ...state,
-                results: updatedArray
-            }
+            // return {
+            //     ...state,
+            //     results: updatedArray
+            // }
+            return updateObject(state, { results: updatedArray })
     }
     return state;
 };
