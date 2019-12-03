8e509f53416da56a0530987bbdc4c2e0482932b3
Aleksandr Beliun <aleksandr.beliun@gmail.com>
2019-12-03 22:44:26 +0100

    minimising switch in reducers

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,6 +5,11 @@ const initialState = {
     results: []
 };
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(state, { results: updatedArray });
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
@@ -19,12 +24,13 @@ const reducer = (state = initialState, action) => {
             // const id = 2;
             // const newArray = [...state.results];
             // newArray.splice(id, 1)
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+            // const updatedArray = state.results.filter(result => result.id !== action.resultElId);
             // return {
             //     ...state,
             //     results: updatedArray
             // }
-            return updateObject(state, { results: updatedArray })
+            // return updateObject(state, { results: updatedArray })
+            return deleteResult(state, action);
     }
     return state;
 };
